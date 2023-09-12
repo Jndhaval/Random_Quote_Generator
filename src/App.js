2ad3c147eb610ae684eebf609b891e119cff9b22
Jndhaval <dhavaljain.414@gmail.com>
2023-09-12 20:51:36 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
   // http://api.quotable.io/random
 
   useEffect(() => {
     fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);  
-          setAuthor(quote.author);
-        }
-      )
-  },[]);
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  }, []);
 
   let fetchNewQuote = () => {
     fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);  
-          setAuthor(quote.author);
-        }
-      )
-  }
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  };
   return (
     <div className="App">
-         <div className="quote">
-            <div className="heading">
-                <h2>{quote}</h2>
-                <small>-{author}-</small>
-            </div>
-            <button className="btn" onClick={fetchNewQuote}>
-                <span>Generate</span>
-            </button>
+      <div className="quote">
+        <div className="heading">
+          <h2>{quote}</h2>
+          <small>-{author}-</small>
         </div>
+        <button className="btn" onClick={fetchNewQuote}>
+          <span>Generate Quote</span>
+        </button>
+      </div>
     </div>
   );
 }
